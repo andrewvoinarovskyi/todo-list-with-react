6cfde8f1dc26624b579d37e66ef4cc72ebdaeec3
Андрій Войнаровський <avoinarovskiy@gmail.com>
2021-05-09 23:37:55 +0300

    add task now with date

--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import {useDispatch} from "react-redux";
-import {ADD_TASK, addNewTask} from "../store/tasks/actions";
+import { useDispatch } from "react-redux";
+import { addNewTask } from "../store/tasks/actions";
 
 function NewItemForm ({ listId }) {
 
     const [item, setItem] = useState({
         title: '',
         description: '',
-        date: '',
+        dueDate: '',
     });
 
     const dispatch = useDispatch();
@@ -20,21 +20,21 @@ function NewItemForm ({ listId }) {
 
     return (
         <form
-          id="new-item-form"
-          onSubmit={onSubmitHandler}
-          onChange={(event) => {
-              const {title, description, date} = event.currentTarget;
-              setItem({
-                  title: title.value,
-                  description: description.value,
-                  date: date.value && new Date(date.value)
-              })
-          }}
-          autoComplete="off">
-              <input name="title" type="text" required placeholder="Title" />
-              <input name="description" type="text" placeholder="Description" />
-              <input name="date" type="date" />
-              <button type="submit">Add</button>
+	          id="new-item-form"
+	          onSubmit={onSubmitHandler}
+	          onChange={(event) => {
+	              const {title, description, date} = event.currentTarget;
+	              setItem({
+	                  title: title.value,
+	                  description: description.value,
+	                  dueDate: date.value && new Date(date.value)
+	              })
+	          }}
+	          autoComplete="off">
+	              <input name="title" type="text" required placeholder="Title" />
+	              <input name="description" type="text" placeholder="Description" />
+	              <input name="date" type="date" />
+	              <button type="submit">Add</button>
         </form>
     )
 }
